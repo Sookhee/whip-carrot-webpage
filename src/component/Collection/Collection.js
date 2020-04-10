20cbdfd4c1df80da183b9461e5b977ab0242594c
Sookhee <s2018s38@e-mirim.hs.kr>
2020-04-11 02:27:43 +0900

    animation test 삭제

--- src/component/Collection/Collection.js
+++ src/component/Collection/Collection.js
@@ -11,7 +11,6 @@ class Collection extends Component{
     render(){
         return(
             <div className="collection">
-                <a href="#" className="hvr-float">Float</a>
                 <div className="title">Collect you Carrot!</div>
                     <div className="carrot-wrap">
                         <Carrot/><Carrot/><Carrot/><Carrot/>
