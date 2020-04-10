a65a060338610a8b88f59710c9947056a80d4554
Sookhee <s2018s38@e-mirim.hs.kr>
2020-04-11 00:50:00 +0900

    Quiz axios 연결 - aws ec2 인스턴스 사용 - php

--- src/component/Quiz/Left.js
+++ src/component/Quiz/Left.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
+import { Parallax } from 'react-scroll-parallax';
 import './Left.scss';
 
 class Left extends Component{
     render(){
         return(
             <div className="quiz-left">
-                <div className="img">
-                </div>
+                <Parallax className="custom-class" y={[-20, 20]} amount={0.1}>
+                    <div className="img">
+                    </div>
+                </Parallax>
             </div>
         );
     };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React,{StrictMode} from 'react';
 import ReactDOM from 'react-dom';
+import { ParallaxProvider } from 'react-scroll-parallax';
 import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
@@ -7,9 +8,11 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <StrictMode>
-        <BrowserRouter basename="/">
-            <App/>
-        </BrowserRouter>
+        <ParallaxProvider>
+            <BrowserRouter basename="/">
+                <App/>
+            </BrowserRouter>
+        </ParallaxProvider>
     </StrictMode>,
     document.getElementById('root'));
 
