3aa6e43196ef961f82ce28d6650d67d544f41d78
jiwonAn <s2018s31@e-mirim.hs.kr>
2020-04-11 23:55:43 +0900

    nav Button

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ import Main from './component/Main/Main';
 import Collection from './component/Collection/Collection';
 import Quiz from './component/Quiz/Quiz';
 
+class ButtonNav extends Component {
+  render() {
+    return (
+      <div>
+        <NavLink to="/quiz"><div className="navButton mar">퀴즈</div></NavLink>
+        <NavLink to="/collection"><div className="navButton">도감</div></NavLink>
+      </div>
+    );
+  }
+}
+
 class App extends Component{
   render(){
 
@@ -12,6 +23,7 @@ class App extends Component{
       <div className="app">
         <div className="nav">
           <NavLink to="/" className="home">채채당</NavLink>
+          <ButtonNav/>
         </div>
         <Route path="/" component={Main} exact={true}/>
         <Route path="/collection" component={Collection}/>
