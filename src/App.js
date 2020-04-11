0e04cc3b1101c91cabbca1738ff555586709746b
jiwonAn <s2018s31@e-mirim.hs.kr>
2020-04-12 00:13:04 +0900

    Nav

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
 import React, {Component} from 'react';
 import {Route, NavLink} from 'react-router-dom';
 import './App.css';
+import Nav from './component/Nav/Nav';
 import Main from './component/Main/Main';
 import Collection from './component/Collection/Collection';
 import Quiz from './component/Quiz/Quiz';
-
-class ButtonNav extends Component {
-  render() {
-    return (
-      <div>
-        <NavLink to="/quiz"><div className="navButton mar">퀴즈</div></NavLink>
-        <NavLink to="/collection"><div className="navButton">도감</div></NavLink>
-      </div>
-    );
-  }
-}
 
 class App extends Component{
   render(){
 
     return (
       <div className="app">
-        <div className="nav">
-          <NavLink to="/" className="home">채채당</NavLink>
-          <ButtonNav/>
-        </div>
+        <Route path="/" component={Nav}/>
         <Route path="/" component={Main} exact={true}/>
         <Route path="/collection" component={Collection}/>
         <Route path="/quiz" component={Quiz}/>
