934addfcc11e96c3468e9685278c7ce66dc7a64d
Sookhee <s2018s38@e-mirim.hs.kr>
2020-04-21 17:21:35 +0900

    NotoSansCJKKR 폰트 추가

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { ParallaxProvider } from 'react-scroll-parallax';
 import {BrowserRouter} from 'react-router-dom';
 import './index.css';
+import './font/fonts.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
