40b68d00cb570bebe4f0ca23deb066237081f3af
Sookhee <s2018s38@e-mirim.hs.kr>
2020-04-21 21:14:09 +0900

    Quiz 화면 디자인 적용

--- src/component/Quiz/Quiz.js
+++ src/component/Quiz/Quiz.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
+import {AppContext} from '../../Context';
+import Axios from 'axios';
 import './Quiz.scss';
+import Right from './Right';
 import Intro from '../Intro/Intro';
 
 class Quiz extends Component{
+
     render(){
         return(
-            <div className="quiz">
+            <div>
                 <Intro title="퀴즈" sub="정답을 맞히신 분들께 추첨을 통해 선물을 드립니다!"/>
+                <Right/>
             </div>
         );
     };
