5723c2bfb8834f5688d626c6cf33671c0871418f
Sookhee <s2018s38@e-mirim.hs.kr>
2020-05-20 00:21:14 +0900

    Team&Member css 수정

--- src/component/Team/Member.js
+++ src/component/Team/Member.js
@@ -5,13 +5,30 @@ class Member extends Component{
     render(){
         return(
             <div className="member">
+                
                 <img className="member-img hvr-grow" src={this.props.img}></img>
                 <div className="member-name">{this.props.name}</div>
+                <div className="member-phone">{this.props.major}</div>
                 <div className="member-phone">{this.props.phone}</div>
-                <div className="sns-wrap">
-                    <a href={this.props.github} class="ir_pm git_ico_img box"><div>Github</div></a>
-                    <a href={this.props.instagram} class="ir_pm insta_ico_img box"><div>Instagram</div></a>
-                </div>
+                {
+                    this.props.github == null ?
+                    (
+                        <div className="sns-wrap">
+                            <a href={this.props.facebook}><div class="ir_pm facebook_img icon_box">Github</div></a>
+                            <a href={this.props.instagram}><div className="ir_pm insta_img icon_box">Instagram</div></a>
+                        </div>
+                    )
+                    :
+                    (
+                        <div className="sns-wrap">
+                            <a href={this.props.github}><div class="ir_pm git_img icon_box">Github</div></a>
+                            <a href={this.props.facebook}><div class="ir_pm facebook_img icon_box">Facebook</div></a>
+                            <a href={this.props.instagram}><div className="ir_pm insta_img icon_box">Instagram</div></a>
+                        </div>
+                    )
+                    
+                }
+                
             </div>
         );
     };
--- src/component/Team/Team.js
+++ src/component/Team/Team.js
@@ -10,36 +10,35 @@ class Team extends Component{
         this.state = {
             memberData: [
                 {
-                    name: "김수경", 
+                    name: "김수경",
+                    major: "뉴미디어 소프트웨어과",
                     phone: "010-9396-2036",
                     github: "https://github.com/sugyeongKim",
                     instagram: "https://www.instagram.com/su_gyeong47/",
+                    facebook: "https://www.facebook.com/profile.php?id=100024603741653",
                     img: "https://github.com/Sookhee/whip-carrot-webpage/blob/master/src/img/sugyeong.jpg?raw=true"
                 }, {
                     name: "안지원", 
+                    major: "뉴미디어 소프트웨어과",
                     phone: "010-7663-9705",
                     github: "https://github.com/jiwonan",
                     instagram: "https://www.instagram.com/jeon1_31/",
+                    facebook: "https://www.facebook.com/Anjiwon1",
                     img: "https://github.com/Sookhee/whip-carrot-webpage/blob/master/src/img/jiwon.jpg?raw=true"
                 }, {
                     name: "정민지", 
+                    major: "뉴미디어 소프트웨어과",
                     phone: "010-4010-6613",
                     github: "https://github.com/Sookhee",
                     instagram: "https://www.instagram.com/5ookhee/",
+                    facebook: "https://www.facebook.com/MinjiJeong02",
                     img: "https://github.com/Sookhee/whip-carrot-webpage/blob/master/src/img/minji.jpg?raw=true"
-                },{
-                    name: "정민지", 
-                    phone: "010-4010-6613",
-                    github: "https://github.com/Sookhee",
-                    instagram: "https://www.instagram.com/5ookhee/",
-                    img: "https://github.com/Sookhee/whip-carrot-webpage/blob/master/src/img/minji.jpg?raw=true"
-                }
-                , {
+                }, {
                     name: "서채원", 
-                    phone: "010-4010-6613",
-                    github: "https://github.com/Sookhee",
-                    instagram: "https://www.instagram.com/5ookhee/",
-                    img: "https://github.com/Sookhee/whip-carrot-webpage/blob/master/src/img/minji.jpg?raw=true"
+                    major: "뉴미디어 디자인과",
+                    phone: "010-8833-6746",
+                    facebook: "https://www.facebook.com/profile.php?id=100009601390266",
+                    img: "https://github.com/Sookhee/AppandMe-Website2020/blob/master/src/img/%EC%84%9C%EC%B1%84%EC%9B%90.jpg?raw=true"
                 }
             ]
         }
@@ -48,14 +47,16 @@ class Team extends Component{
     render(){
         return(
             <div className="team">
-                <Intro title="팀원 소개" sub="채채당 개발 및 디자인에 참가한 미림인들입니다"/>
+                <Intro title="팀원 소개" sub="채채당을 만든 개발자들과 디자이너 입니다."/>
                 <div className="wrap-member">
                     {
                     this.state.memberData.map((member, i) => {
                         return (<Member name={member.name}
+                                        major={member.major}
                                         phone={member.phone}
                                         github={member.github}
                                         instagram={member.instagram}
+                                        facebook={member.facebook}
                                         img={member.img}
                                         key={i} 
                             />);
