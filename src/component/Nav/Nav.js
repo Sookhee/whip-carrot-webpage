d87ff58f063130f23189ef8e47bd6222d5b4483c
Sookhee <s2018s38@e-mirim.hs.kr>
2020-05-19 23:31:08 +0900

    Nav css 수정

--- src/component/Nav/Nav.js
+++ src/component/Nav/Nav.js
@@ -8,9 +8,9 @@ class Nav extends Component {
         <div className="nav">  
             <NavLink to="/" className="home">채찍채찍 당근</NavLink>
               <div className="Btndesign">
-                <NavLink to="/quiz" activeStyle={{color:'#ff8448'}} className="navButton">퀴즈</NavLink>
                 <NavLink to="/collection" activeStyle={{color:'#ff8448'}} className="navButton">당근 도감</NavLink>
                 <NavLink to="/member" activeStyle={{color:'#ff8448'}} className="navButton">팀원 소개</NavLink>
+                <NavLink to="/quiz" activeStyle={{color:'#ff8448'}} className="navButton">퀴즈</NavLink>
               </div>
           </div>
       );
