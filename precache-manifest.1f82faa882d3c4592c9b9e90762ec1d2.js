2f26a89024e1d451b820951822e2b2907fdd509c
Sookhee <s2018s38@e-mirim.hs.kr>
2020-07-06 23:08:32 +0900

    Updates

---
+++ precache-manifest.1f82faa882d3c4592c9b9e90762ec1d2.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f830526a9b1f66fb43614230f82618e",
+    "url": "/whip-carrot-webpage/index.html"
+  },
+  {
+    "revision": "87f665ca488419b85ad4",
+    "url": "/whip-carrot-webpage/static/css/main.c20505ce.chunk.css"
+  },
+  {
+    "revision": "bcac44b00446073e0938",
+    "url": "/whip-carrot-webpage/static/js/2.cbc27e71.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/whip-carrot-webpage/static/js/2.cbc27e71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87f665ca488419b85ad4",
+    "url": "/whip-carrot-webpage/static/js/main.ef773709.chunk.js"
+  },
+  {
+    "revision": "c345a9a70d859fe9a979",
+    "url": "/whip-carrot-webpage/static/js/runtime-main.5088a335.js"
+  },
+  {
+    "revision": "79d613fb8ec20fbb6bfcbf64e93cc9b2",
+    "url": "/whip-carrot-webpage/static/media/NotoSansCJKkr-Bold.79d613fb.otf"
+  },
+  {
+    "revision": "a59504711a5c86e8e03b675bfeb0be76",
+    "url": "/whip-carrot-webpage/static/media/NotoSansCJKkr-Medium.a5950471.otf"
+  },
+  {
+    "revision": "19b7d8d4011e8f88930022704558246d",
+    "url": "/whip-carrot-webpage/static/media/NotoSansCJKkr-Regular.19b7d8d4.otf"
+  },
+  {
+    "revision": "d7f5b6afac3082dac4e05748bf3c4be5",
+    "url": "/whip-carrot-webpage/static/media/carrot1.d7f5b6af.png"
+  },
+  {
+    "revision": "e64faa0fcf02c728fe29f963ff607b94",
+    "url": "/whip-carrot-webpage/static/media/carrot10.e64faa0f.png"
+  },
+  {
+    "revision": "00514e0a5926c8c81ecbae7886d332d2",
+    "url": "/whip-carrot-webpage/static/media/carrot2.00514e0a.png"
+  },
+  {
+    "revision": "6a921a8751f7bfde91d5523b92b1e148",
+    "url": "/whip-carrot-webpage/static/media/carrot3.6a921a87.png"
+  },
+  {
+    "revision": "9d168f5cbb8b8f13a11fd995304a82c9",
+    "url": "/whip-carrot-webpage/static/media/carrot4.9d168f5c.png"
+  },
+  {
+    "revision": "5d1eace2aa8f6bf7a272bc0fb01f97be",
+    "url": "/whip-carrot-webpage/static/media/carrot6.5d1eace2.png"
+  },
+  {
+    "revision": "9f4d29a62580737b9f6b117934fe6430",
+    "url": "/whip-carrot-webpage/static/media/carrot7.9f4d29a6.png"
+  },
+  {
+    "revision": "ec7274ede1404bdea20c632d57f7424d",
+    "url": "/whip-carrot-webpage/static/media/carrot9.ec7274ed.png"
+  },
+  {
+    "revision": "f325dd91c860c69b574ab204c0dd9045",
+    "url": "/whip-carrot-webpage/static/media/jiwon.f325dd91.jpg"
+  },
+  {
+    "revision": "2fa96c3928fd2a1987fac535fa533e99",
+    "url": "/whip-carrot-webpage/static/media/main-backgronud.2fa96c39.png"
+  },
+  {
+    "revision": "cf25a35a77c5773a30f969b7fda1ac44",
+    "url": "/whip-carrot-webpage/static/media/minji.cf25a35a.jpg"
+  },
+  {
+    "revision": "6aa6a7a148f00d2878c1a0b9f28037c6",
+    "url": "/whip-carrot-webpage/static/media/sugyeong.6aa6a7a1.jpg"
+  }
+]);
